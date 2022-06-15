cff45f7c2e20dbd929c488869d61b7945fba5b58
Songhj03 <105920534+Songhj03@users.noreply.github.com>
2022-06-16 02:26:23 +0900

    엔딩 페이지

---
+++ Core/Assets/Scripts/GameOver.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    float time = 0;
+
+    void Update()
+    {
+        if (time < 3f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, time/3);
+        }
+        else if (time > 5f && Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Startpage");
+        }
+        time += Time.deltaTime;
+    }
+}
